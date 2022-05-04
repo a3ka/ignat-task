d4e23b170955476fff3959d06e84a0b0e8c7070f
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-05-04 21:52:02 +0300

    8w_correct+9w

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,36 +1,39 @@
 import {UserType} from "../HW8";
 
-export type homeWorkReducerType = sortUpACType | sortDownACType | checkACType
+export type homeWorkReducerType = sortACType | checkACType
 
-export type sortUpACType = ReturnType<typeof sortUpAC>
-export type sortDownACType = ReturnType<typeof sortDownAC>
+export type sortACType = {
+    type: 'sort',
+    payload: 'up' | 'down'
+}
+// export type sortACType = ReturnType<typeof sortAC>
+// export type sortDownACType = ReturnType<typeof sortDownAC>
 export type checkACType = ReturnType<typeof checkAC>
 
 
 export const homeWorkReducer = (state: Array<UserType>, action: homeWorkReducerType): Array<UserType> => {
     switch (action.type) {
-        case 'sortUp': {
-            return state.sort(function(a, b){
-                let nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
-                if (nameA < nameB) //sort string ascending
-                    return -1;
-                if (nameA > nameB)
-                    return 1;
-                return 0; //default return value (no sorting)
-            });
+        case 'sort': {
+            const newState = [...state].sort((a:UserType, b) => {
+                if (a.name > b.name) return 1
+                else if (a.name < b.name) return -1
+                else return 0
+            })
+
+            return action.payload === "up" ? newState : newState.reverse()
         }
-        case 'sortDown': {
-            return state.sort(function(a, b){
-                let nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
-                if (nameA < nameB) //sort string ascending
-                    return 1;
-                if (nameA > nameB)
-                    return -1;
-                return 0; //default return value (no sorting)
-            });
-        }
+        // case 'sortDown': {
+        //     return state.sort(function(a, b){
+        //         let nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
+        //         if (nameA < nameB) //sort string ascending
+        //             return 1;
+        //         if (nameA > nameB)
+        //             return -1;
+        //         return 0; //default return value (no sorting)
+        //     });
+        // }
         case 'check': {
-            return state.filter(el => el.age > action.payload)
+            return state.filter(el => el.age >= action.payload)
         }
         default: return state
     }
@@ -38,14 +41,14 @@ export const homeWorkReducer = (state: Array<UserType>, action: homeWorkReducerT
 
 export const sortUpAC = () => {
     return {
-        type: 'sortUp',
+        type: 'sort',
         payload: 'up'
     } as const
 }
 
 export const sortDownAC = () => {
     return {
-        type: 'sortDown',
+        type: 'sort',
         payload: 'down'
     } as const
 }
--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -18,18 +18,22 @@ test('sort name up', () => {
     // const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
     const newState = homeWorkReducer(initialState, sortUpAC())
 
-    console.log(newState)
-    expect(newState).toStrictEqual(
-        [
-            {_id: 1, name: 'Александр', age: 66},
-            {_id: 3, name: 'Виктор', age: 44},
-            {_id: 4, name: 'Дмитрий', age: 40},
-            {_id: 5, name: 'Ирина', age: 55},
-            {_id: 2, name: 'Коля', age: 16},
-            {_id: 0, name: 'Кот', age: 3},
-        ]
-    )
+    const result = [
+        {_id: 1, name: 'Александр', age: 66},
+        {_id: 3, name: 'Виктор', age: 44},
+        {_id: 4, name: 'Дмитрий', age: 40},
+        {_id: 5, name: 'Ирина', age: 55},
+        {_id: 2, name: 'Коля', age: 16},
+        {_id: 0, name: 'Кот', age: 3},
+    ]
+
+    expect(newState).toStrictEqual(result)
     expect(newState.length).toBe(6)
+    expect(newState[0]._id).toBe(1)
+
+
+
+
 })
 
 
