39a603c12f3acf254eb2aec578bda64269fbf498
Oleksandr Kalynovskyy <alex.kurz@protonmail.com>
2022-05-27 15:41:21 +0300

    12w

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,19 +1,36 @@
 import React from "react";
 import s from "./HW12.module.css";
+import SuperSelect from "../h7/common/c5-SuperSelect/SuperSelect";
+import {useDispatch, useSelector} from "react-redux";
+import {AppStoreType} from "../h10/bll/store";
+import {changeThemeAC, InitialStateType} from "./bll/themeReducer";
 
 const themes = ['dark', 'red', 'some'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
-
-    // useDispatch, onChangeCallback
+    // const theme = 'some'; // useSelector
+    const theme = useSelector<AppStoreType, string> (state => state.theme.color)
+    const dispatch = useDispatch();
+    const changeThemeCB = (option: string) => {
+        dispatch(changeThemeAC(option))
+    }
 
     return (
         <div className={s[theme]}>
+
+            {console.log(theme)}
             <hr/>
             <span className={s[theme + '-text']}>
-                homeworks 12
+               <h1>homeworks 12</h1>
             </span>
+
+            <div>
+                <SuperSelect
+                    options={themes}
+                    // value={value}
+                    onChangeOption={(option) => changeThemeCB(option)}
+                />
+            </div>
 
             {/*should work (должно работать)*/}
             {/*SuperSelect or SuperRadio*/}
--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,14 +1,31 @@
-const initState = {
-
+export type InitialStateType = {
+    color: string
 };
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+const initState: InitialStateType = {
+    color: "white"
+};
+
+
+type ActionType = ChangeThemeType;
+type ChangeThemeType = ReturnType<typeof changeThemeAC>
+
+
+export const themeReducer = (state = initState, action: ActionType): InitialStateType => {
     switch (action.type) {
-        case "": {
-            return state;
+        case "CHANGE-THEME": {
+            return {...state, color: action.payload.option};
         }
-        default: return state;
+        default:
+            return state;
     }
 };
 
-export const changeThemeC = (): any => {}; // fix any
+export const changeThemeAC = (option: string) => {
+    return {
+        type: "CHANGE-THEME",
+        payload: {option},
+    } as const
+};
+
+
